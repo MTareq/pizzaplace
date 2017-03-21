526bbd879460e1e72729cf891c7cd43ef7ee06c6
mtareq <m.tareq89@gmail.com>
2017-03-21 13:25:58 +0200

    Pizza Iniatial Fixture Test, refer to #2

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,13 @@
+import ipdb
 from django.test import TestCase
+from django.core.management import call_command
+from orders.models import Pizza
 
-# Create your tests here.
+
+class PizzaTestCase(TestCase):
+
+    def test_pizza_initial_data(self):
+        call_command("loaddata", "pizza.json", verbosity=0)
+        fun_guy_pizza = Pizza.objects.get(id=6)
+        self.assertEqual(fun_guy_pizza.name, 'Funghi')
+
